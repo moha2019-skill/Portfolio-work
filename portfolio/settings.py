cb28867effbfa501d55bd10c093167fbabc7918b
Moha Gad <moha.gad@smu.ca>
2019-10-28 17:17:14 -0300

    Blog and polish

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER':'postgres',
-        'PASSWORD': 'Ihatethisc2',
+        'PASSWORD': 'lifeisgreat',
         'HOST': 'localhost',
         'PORT':'5432',
     }
